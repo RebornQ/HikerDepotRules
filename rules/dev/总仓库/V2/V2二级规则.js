0fed8e5b57f6620793b133828fad44e773f90dd4
Reborn <ren_xiaoyao@outlook.com>
2020-07-04 22:40:57 +0800

    fix: 总仓库V2: 修复无法检测规则类型时不显示提示的问题

--- rules/dev/总仓库/V2/V2二级规则.js
+++ rules/dev/总仓库/V2/V2二级规则.js
@@ -392,30 +392,15 @@ if (getUrl().indexOf("rule://") != -1) {
                             rule.updateText == null ?
                                 "无"
                                 : rule.updateText
-                        )
-                        + (
-                            rule.tips != null && rule.tips != "" ?
-                                "<br><br>Tips: " + rule.tips
-                                : ""
                         ));
             } else {
                 return rule.oldVersion > rule.version ?
                     "‘‘喵？为啥你的规则版本比我还高？’’"
-                    : "当前规则已是最新版，点击跳到规则页"
-                    + (
-                        rule.tips != null && rule.tips != "" ?
-                            "\n\nTips: " + rule.tips
-                            : ""
-                    );
+                    : "当前规则已是最新版，点击跳到规则页";
             }
         } else {
             depotRulesStatus.noImportNum += 1;
-            return "‘‘你尚未导入该规则’’，点击导入"
-                + (
-                    rule.tips != null && rule.tips != "" ?
-                        "<br><br>Tips: " + rule.tips :
-                        ""
-                );
+            return "‘‘你尚未导入该规则’’，点击导入";
         }
     };
 
@@ -651,6 +636,11 @@ if (getUrl().indexOf("rule://") != -1) {
                 } else {
                     r.desc = desc(myRules, j);
                 }
+                r.desc = r.desc + (
+                    j.tips != null && j.tips != "" ?
+                        "\n\nTips: " + j.tips
+                        : ""
+                );
                 r.title = j.mappingTitle != null && j.mappingTitle != "" && j.isMapped == true ?
                     j.mappingTitle
                     : j.title;
