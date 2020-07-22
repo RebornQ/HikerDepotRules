4fbfa868a28a3ea32639cf95235af1d2e2c1c93d
Reborn <ren_xiaoyao@outlook.com>
2020-07-22 09:54:47 +0800

    修复：删除文件脚本找不到文件导致运行失败的问题

--- scripts/clean_scripts.sh
+++ scripts/clean_scripts.sh
@@ -2,7 +2,11 @@
 
 target=$1
 echo $target
-rm $target/merge_rules.sh
-rm $target/merge.sh
-rm $target/merge_classes.sh
-rm $target/temp
+del_files=($target/merge_rules.sh $target/merge.sh $target/merge_classes.sh $target/temp)
+for file in "${del_files[@]}"; do         # iterate over the rest
+  if [ ! -f "$file" ]; then
+    echo "File not found"
+  else
+    rm "$file"
+  fi
+done
